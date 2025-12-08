f5306bab12fbc972457e5c5c784be38a39d4c84c
darrenlu <2945034270@qq.com>
2025-12-08 23:03:49 +0800

    add install script

---
+++ install.sh
@@ -0,0 +1,3 @@
+pip install diffusers==0.33.0 -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
+export EXT_PARALLEL=4 NVCC_APPEND_FLAGS="--threads 8" MAX_JOBS=32 # Optional
+python setup.py develop
